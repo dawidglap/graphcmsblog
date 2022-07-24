d3987ddf6ef6706c0c98dd6eb09a90c03ad12a3f
Dawid Glapiak <dg18@live.it>
2022-07-24 19:34:44 +0200

    completed comment component

--- services/index.js
+++ services/index.js
@@ -167,3 +167,22 @@ export const getSimilarPosts = async (categories, slug) => {
 
   }
 
+
+
+  export const getComments = async(slug) => {
+    const query = gql`
+      query GetComments($slug: String!) {
+        comments(where: {post: {slug: $slug}}) {
+          name
+          createdAt
+          comment
+        }
+      }
+    `
+
+    const result = await request(graphqlAPI, query, {slug});
+
+    return result.comments;
+  }
+
+
